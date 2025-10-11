0be9929d1851e34af4b1724b025a18725bc85870
sehkone <sehkone@cluml.com>
2025-10-11 09:06:02 +0900

    Add node-friendly activity API route coverage
    Part of issue #103

--- tests/setup/resize-observer.ts
+++ tests/setup/resize-observer.ts
@@ -1,3 +1,12 @@
+if (typeof Element === "undefined") {
+  class MockElement {}
+  Object.defineProperty(globalThis, "Element", {
+    configurable: true,
+    writable: true,
+    value: MockElement,
+  });
+}
+
 class MockResizeObserver implements ResizeObserver {
   private readonly callback: ResizeObserverCallback;
 
@@ -48,27 +57,29 @@ Object.defineProperty(globalThis, "ResizeObserver", {
   value: MockResizeObserver,
 });
 
-const originalGetBoundingClientRect = Element.prototype.getBoundingClientRect;
+if (typeof Element !== "undefined") {
+  const originalGetBoundingClientRect = Element.prototype.getBoundingClientRect;
 
-Element.prototype.getBoundingClientRect = function getBoundingClientRect() {
-  if (originalGetBoundingClientRect) {
-    const rect = originalGetBoundingClientRect.call(this);
-    if (rect?.width && rect?.height) {
-      return rect;
+  Element.prototype.getBoundingClientRect = function getBoundingClientRect() {
+    if (originalGetBoundingClientRect) {
+      const rect = originalGetBoundingClientRect.call(this);
+      if (rect?.width && rect?.height) {
+        return rect;
+      }
     }
-  }
 
-  return {
-    x: 0,
-    y: 0,
-    top: 0,
-    left: 0,
-    right: 800,
-    bottom: 600,
-    width: 800,
-    height: 600,
-    toJSON() {
-      return {};
-    },
-  } satisfies DOMRect;
-};
+    return {
+      x: 0,
+      y: 0,
+      top: 0,
+      left: 0,
+      right: 800,
+      bottom: 600,
+      width: 800,
+      height: 600,
+      toJSON() {
+        return {};
+      },
+    } satisfies DOMRect;
+  };
+}
